41cfeecd2a16c073506b5b9b89f9878173a3534e
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-02 17:39:31 -0700

    add fetch

--- app/page.js
+++ app/page.js
@@ -8,6 +8,19 @@ import Tag from '../component/cards/Tag'
 export default function Home() {
   const [ posts , setPosts ] = useState([])
   const [ selectedTags, setSelectedTags ] = useState([])
+  const [ error, setError ] = useState(null)
+
+  useEffect(()=>{
+    async function fetchData(){
+      try{
+        const data = await axios.get('/api/posts')
+        setPosts(data.data)
+      }catch(error){
+        setError('data not fetching')
+      }
+    }
+  },[])
+
   return (
     <div>
       

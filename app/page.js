0629a6892a47c6294df880e546dadba79dc1ced8
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-02 18:55:44 -0700

    add tag

--- app/page.js
+++ app/page.js
@@ -6,24 +6,31 @@ import axios from 'axios'
 import Tag from '../component/cards/Tag'
 
 export default function Home() {
-  const [ posts , setPosts ] = useState([])
+  const [ posts , setPosts ] = useState([
+    {
+      title:'new technology learn',
+      subtitle: 'here is learn some next.js and mongoose',
+      image: 'https://miro.medium.com/v2/resize:fit:1200/1*y6C4nSvy2Woe0m7bWEn4BA.png',
+      tag: 'react.js',
+    }
+  ])
   const [ selectedTags, setSelectedTags ] = useState([])
   const [ error, setError ] = useState(null)
 
-  useEffect(()=>{
-    async function fetchData(){
-      try{
-        const data = await axios.get('/api/posts')
-        setPosts(data.data)
-      }catch(error){
-        setError('data not fetching')
-      }
-    }
-  },[])
+  // useEffect(()=>{
+  //   async function fetchData(){
+  //     try{
+  //       const data = await axios.get('/api/posts')
+  //       setPosts(data.data)
+  //     }catch(error){
+  //       setError('data not fetching')
+  //     }
+  //   }
+  // },[])
 
-  // if(posts?.length === 0){
-  //   return <div className='text-center'>no post found...</div>
-  // }
+  if(posts?.length === 0){
+    return <div className='text-center'>no post found...</div>
+  }
   // if(error){
   //   return (
   //     <div className='text-center text-4xl mt-10'>
@@ -35,11 +42,24 @@ export default function Home() {
 
   return (
     <main className='min-h-screen p-10 lg:p-20 '>
-      <h2 className='text-center text-3xl md:text-4xl lg:text-5xl font-bold mb-10 flex justify-center items-center'>
-        <span className='text-5xl border-b-4 pb-3 font-bold'>
+      <h2 className='text-center text-3xl md:text-4xl lg:text-5xl font-bold mb-10 flex flex-col justify-center items-center uppercase tracking-widest h-96 '>
+        <span className='text-4xl border-b-4 pb-3 font-bold'>
           Md. Tufael Khan Web Dev Blog
         </span>
       <p className='text-lg mt-10 '>Like, share and subscribe for more content</p>
+      </h2>
+      <h2 className='flex flex-wrap mt-10 gap-4 '>
+        {
+          [
+            ...new Set(
+              posts?.map((post, idx) =>{
+                return post.tag
+              })
+            )
+          ].map(tag =>{
+            return <div>{tag}</div>
+          })
+        }
       </h2>
     </main>
   )
---
+++ component/cards/Tag.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Tag = ({
+    tag,
+    isSelected,
+    setSelectedTags,
+}) => {
+
+    if(isSelected){
+        return (
+            <div onClick={()=> setSelectedTags((prev) => prev.filter(item => item !== tag))} className='bg-green-600 text-white font-semibold py-1 px-2 rounded-lg hover:shadow-lg transition duration-300 ease-in-out flex items-center justify-center'>
+                {tag}
+            </div>
+        )
+    }
+
+  return (
+    <div>Tag</div>
+  )
+}

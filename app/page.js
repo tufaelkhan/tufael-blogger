c469a7eea7f18704473a45b9f994d183632c5b28
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-02 17:33:48 -0700

    add adat

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 import Tag from '../component/cards/Tag'
 
 export default function Home() {
+  const [ posts , setPosts ] = useState([])
+  const [ selectedTags, setSelectedTags ] = useState([])
   return (
     <div>
       

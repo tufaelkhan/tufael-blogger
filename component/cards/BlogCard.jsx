20979e8d7080d5c1f7393531ccaa257141dc1e74
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-03 09:40:55 -0700

    add style

--- component/cards/BlogCard.jsx
+++ component/cards/BlogCard.jsx
@@ -7,6 +7,12 @@ export const BlogCard = ({ tag, title, img, id}) => {
         <Link href={`/post/${id}`}>
             <div className='rounded-lg object-cover'>
                 <img src={img} alt={title} className='rounded-t-lg object-cover h-40 w-full ' />
+                <div className="p-4">
+                    <div className="text-sm font-medium text-gray-400">
+                        {tag}
+                    </div>
+                    <p className='text-xl font-semibold'></p>
+                </div>
             </div>
         </Link>
     </div>

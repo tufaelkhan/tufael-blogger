fbb1cac127ba6d738832dce47449d6ca77b6a37c
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-05 08:54:35 -0700

    add data

--- component/Nav.jsx
+++ component/Nav.jsx
@@ -13,14 +13,16 @@ export const Nav = ({posts}) => {
   return (
     <nav className='px-16 mt-10 flex justify-between items-center flex-wrap'>
         <h1 className='text-4xl font-bold text-gray-800 p-4 mx-auto md:mx-0 '>
-            tufael khan web dev
+            tufael khan web dev latest technology blogs
         </h1>
         <div className="relative mx-auto md:mx-0 ">
         <input type='text'
-        className='rounded-lg border-2 border-gray-200 focus:outline-none'
-        >
+        className='rounded-lg border-2 border-gray-200 focus:outline-none focus:border-green-600 transition duration-300 ease-in-out px-4 py-2'
+        placeholder='search'
+        aria-label='search'
+        onChange={(e)=> setFilter(e.target.value)}
+        />
 
-        </input>
         </div>
     </nav>
   )
